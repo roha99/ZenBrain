40a185b6797567478c47bbdd4a5032fffef84624
r0b3rth4ns3n <r0b3rth4ns3n@protonmail.com>
2020-12-09 18:54:25 +0100

    Initial Commit

---
+++ ZenBrain/ZenBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenBrainApp.swift
+//  ZenBrain
+//
+//  Created by Robert Hansen on 09.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ZenBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
